124476da410fe76a4dcc31b8ec8ce2f40935b292
helloworld <ura22051995@gmail.com>
2022-11-14 22:20:58 +0300

    Выполнил ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        // Задача 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        // Задача 3
+        String fullNameRus = "Иванов Семён Семёнович";
+        System.out.println(fullNameRus.replace('ё', 'е'));
     }
 }
